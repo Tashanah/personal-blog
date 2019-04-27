42b1631d5c3d80e06c5f59a56f5968141bd634f0
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-04-27 14:32:12 +0300

    blog class created

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from .import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class Blog(db.Model):
+    '''
+    Blog class to define the blogs
+    '''
+   __tablename__ = 'blogs'
+    id = db.Column(db.Integer, primary_key=True)
+    blog = db.Column(db.String)
+    submitted_by = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+  
