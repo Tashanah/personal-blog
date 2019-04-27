bd28e30914968163582324dc0f660fe9138488fe
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-04-27 15:04:14 +0300

    user  class created

--- app/models.py
+++ app/models.py
@@ -67,3 +67,37 @@ class Review(db.Model):
                 response.append(review)
 
         return response
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), index=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    # pitches=db.relationship('Pitch', backref='user', lazy="dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    # def save_user(self):
+    #     db.session.add(self)
+    #     db.session.commit()
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+
+
