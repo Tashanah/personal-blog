f52dfdbcac5c1fba40e1bb7f815c0aaf1f04e8cd
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-04-27 17:07:43 +0300

    function to process quote results and return as an object

--- app/requests.py
+++ app/requests.py
@@ -2,7 +2,7 @@ import urllib.request,json
 from .models import Quote
 
 
-
+#Quotes=quotes.Quotes
 # Getting api key
 api_key = None
 # Getting the quotes base url
@@ -17,17 +17,35 @@ def get_quotes(category):
     '''
     Function that gets the json response to our url request
     '''
-    get_quotess_url = base_url.format(category,api_key)
+    get_quotes_url = base_url.format(category,api_key)
     
     with urllib.request.urlopen(get_movies_url) as url:
-        get_quotess_data = url.read()
-        get_quotess_response = json.loads(get_quotes_data)
+        get_quotes_data = url.read()
+        get_quotes_response = json.loads(get_quotes_data)
         
         quotes_results = None
         
         if get_quotes_response['results']:
             quote_results_list = get_quotes_response['results']
-            quotee_results = process_results(quote_results_list)
+            quote_results = process_results(quote_results_list)
             
         return quote_results
         
+def process_results(quote_list):
+    '''
+    Function  that processes the quote result and transform them to a list of Objects
+
+    Args:
+        quote_list: A list of dictionaries that contain quote details
+
+    Returns :
+        quote_results: A list of quote objects
+    '''
+    quote_results = []
+    for quote_item in quote_list:
+        id = movie_item.get('id')
+        author = quote_item.get('author')
+        quote = quote_item.get('quote')
+    
+
+    return quote_results
